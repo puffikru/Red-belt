df84a3060299f0e43107556b4534fc8756a25750
Igor Bulakh <bulakhigor@yandex-team.ru>
2019-03-19 22:31:07 +0300

    class template

--- class_template.cpp
+++ class_template.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
@@ -25,11 +26,6 @@ IteratorRange<typename vector<T>::iterator> Head(vector<T>& v, size_t top) {
     };
 }
 
-template <typename T>
-size_t RangeSize(IteratorRange<T> r) {
-    return r.end() - r.begin();
-}
-
 
 int main() {
     vector<int> v = {1, 2, 3, 4, 5};
@@ -39,7 +35,38 @@ int main() {
     for (int x : v) {
         cout << x << ' ';
     }
-    cout << endl;
-    cout << RangeSize(Head(v, 3));
     return 0;
 }
+
+
+/*struct PairOfStringAndInt {
+    string first;
+    int second;
+};
+
+struct PairOfBoolAndChar {
+    bool first;
+    char second;
+};
+
+
+template <typename T, typename U>
+struct Pair {
+    T first;
+    U second;
+};
+
+
+int main() {
+    Pair<string, int> si;
+    si.first = "Hello";
+    si.second = 5;
+
+    Pair<bool, char> bc;
+    bc.first = true;
+    bc.second = 'z';
+
+    return 0;
+}*/
+
+
